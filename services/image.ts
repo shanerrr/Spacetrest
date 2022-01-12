579567a5a49de86a6bcf4b9e5eb6767e295a0181
Shane Goonasekera <theshanukg@gmail.com>
2022-01-11 20:50:18 -0700

    serverlles timeout error vercel fix

--- services/image.ts
+++ services/image.ts
@@ -21,7 +21,7 @@ export class ImageService {
     return axios({
       method: 'GET',
       headers: { 'Content-Type': 'application/json' },
-      timeout: startDate ? 30000 : 10000,
+      timeout: startDate ? 60000 : 4500,
       url: this.uri + this.config.pictureOfDay + `${startDate ? `&start_date=${startDate}&end_date=${endDate}` : ''}`
     });
   }
