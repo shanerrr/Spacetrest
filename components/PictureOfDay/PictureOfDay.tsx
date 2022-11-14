b0d1d1c1c45e59f1db384f45fbde74844688c1af
Shane Goonasekera <theshanukg@gmail.com>
2022-11-13 20:25:58 -0700

    added legacy imports

--- components/PictureOfDay/PictureOfDay.tsx
+++ components/PictureOfDay/PictureOfDay.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Image from "next/legacy/image";
+import Link from "next/link";
 
-import type { image } from '../../types/image.interface';
+import type { image } from "../../types/image.interface";
 
-const PictureOfDay = ({ imageResponse, isLiked, onLikeClick, scrollToGallery }: { imageResponse: image, isLiked: boolean, onLikeClick: (url: string, event: React.MouseEvent<HTMLElement>) => void, scrollToGallery: (() => void) | undefined }) => {
-
+const PictureOfDay = ({
+  imageResponse,
+  isLiked,
+  onLikeClick,
+  scrollToGallery,
+}: {
+  imageResponse: image;
+  isLiked: boolean;
+  onLikeClick: (url: string, event: React.MouseEvent<HTMLElement>) => void;
+  scrollToGallery: (() => void) | undefined;
+}) => {
   //to show information & deal with parralax
-  const [showPictureofDay, setShowPictureofDay] = React.useState(imageResponse.show);
+  const [showPictureofDay, setShowPictureofDay] = React.useState(
+    imageResponse.show
+  );
   const [offsetY, setOffsetY] = React.useState(0);
 
   // callback to update scroll position
@@ -22,57 +33,121 @@ const PictureOfDay = ({ imageResponse, isLiked, onLikeClick, scrollToGallery }: 
 
   return (
     <>
-      <div style={{ transform: `translateY(${offsetY * 0.4}px)` }} className={`${showPictureofDay ? 'h-[94%]' : 'h-full'} relative`}>
-        {
-          imageResponse.media_type === 'image' ?
-            <Image alt={imageResponse.date + ' APOD'} className={`${showPictureofDay ? 'rounded-b-[30px]' : 'rounded-none'} transition-all duration-300`} placeholder='blur' src={imageResponse.title} loader={() => imageResponse.hdurl} layout='fill' objectFit='cover' quality={100} priority={true} blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkMAYAADkANVKH3ScAAAAASUVORK5CYII=" />
-            :
-            <iframe
-              className='rounded-b-[30px]'
-              width="100%"
-              height="100%"
-              src={imageResponse.url + '&autoplay=1&controls=0&loop=1&playlist=' + imageResponse.url.split('https://www.youtube.com/embed/')[1].split('?rel=0')[0]}
-              frameBorder="0"
-              title="Embedded youtube"
-            />
-        }
+      <div
+        style={{ transform: `translateY(${offsetY * 0.4}px)` }}
+        className={`${showPictureofDay ? "h-[94%]" : "h-full"} relative`}
+      >
+        {imageResponse.media_type === "image" ? (
+          <Image
+            alt={imageResponse.date + " APOD"}
+            className={`${
+              showPictureofDay ? "rounded-b-[30px]" : "rounded-none"
+            } transition-all duration-300`}
+            placeholder="blur"
+            src={imageResponse.title}
+            loader={() => imageResponse.hdurl}
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            priority={true}
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkMAYAADkANVKH3ScAAAAASUVORK5CYII="
+          />
+        ) : (
+          <iframe
+            className="rounded-b-[30px]"
+            width="100%"
+            height="100%"
+            src={
+              imageResponse.url +
+              "&autoplay=1&controls=0&loop=1&playlist=" +
+              imageResponse.url
+                .split("https://www.youtube.com/embed/")[1]
+                .split("?rel=0")[0]
+            }
+            frameBorder="0"
+            title="Embedded youtube"
+          />
+        )}
       </div>
 
-      <section className='absolute p-8 bottom-0 drop-shadow-2xl animate-fadeInBottom'>
-        <div className='grid grid-cols-1 xl:grid-cols-2 gap-4'>
-          <div className={`bg-black lg:bg-black/80 rounded-lg drop-shadow-2xl max-h-[536px] lg:max-h-fit overflow-y-auto p-6 ${showPictureofDay ? 'visible' : 'invisible'}`}>
-            <span className='text-white'>{new Date(imageResponse.date).toLocaleDateString("en", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC' })}</span>
-            <h1 className='font-bold text-white text-4xl md:text-6xl align-middle underline pt-1'><a href={imageResponse.hdurl} target='_blank' rel="noreferrer">{imageResponse.title}</a></h1>
-            <p className='text-white font-medium pt-7'>{imageResponse.explanation}</p>
+      <section className="absolute p-8 bottom-0 drop-shadow-2xl animate-fadeInBottom">
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
+          <div
+            className={`bg-black lg:bg-black/80 rounded-lg drop-shadow-2xl max-h-[536px] lg:max-h-fit overflow-y-auto p-6 ${
+              showPictureofDay ? "visible" : "invisible"
+            }`}
+          >
+            <span className="text-white">
+              {new Date(imageResponse.date).toLocaleDateString("en", {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                timeZone: "UTC",
+              })}
+            </span>
+            <h1 className="font-bold text-white text-4xl md:text-6xl align-middle underline pt-1">
+              <a href={imageResponse.hdurl} target="_blank" rel="noreferrer">
+                {imageResponse.title}
+              </a>
+            </h1>
+            <p className="text-white font-medium pt-7">
+              {imageResponse.explanation}
+            </p>
           </div>
-          <div className='flex justify-center lg:justify-end items-end'>
-            <div className='flex'>
-              {
-                scrollToGallery ?
-                  <div title='Scroll down' className='flex items-center justify-center rounded-full p-2 lg:p-3 bg-[#181A18] w-12 h-12 md:w-16 md:h-16 mr-2 drop-shadow-2xl border-white border-2 hover:border-green-500 transition-all duration-200 cursor-pointer' onClick={scrollToGallery}>
-                    <i className={`uil uil-arrow-down text-3xl md:text-4xl text-white`}></i>
+          <div className="flex justify-center lg:justify-end items-end">
+            <div className="flex">
+              {scrollToGallery ? (
+                <div
+                  title="Scroll down"
+                  className="flex items-center justify-center rounded-full p-2 lg:p-3 bg-[#181A18] w-12 h-12 md:w-16 md:h-16 mr-2 drop-shadow-2xl border-white border-2 hover:border-green-500 transition-all duration-200 cursor-pointer"
+                  onClick={scrollToGallery}
+                >
+                  <i
+                    className={`uil uil-arrow-down text-3xl md:text-4xl text-white`}
+                  ></i>
+                </div>
+              ) : (
+                <Link href="/">
+                  <div
+                    title="Home"
+                    className="flex items-center justify-center rounded-full p-2 lg:p-3 bg-[#181A18] w-12 h-12 md:w-16 md:h-16 mr-2 drop-shadow-2xl border-white border-2 hover:border-green-500 transition-all duration-200 cursor-pointer"
+                    onClick={scrollToGallery}
+                  >
+                    <i
+                      className={`"uil uil-home text-3xl md:text-4xl text-white`}
+                    ></i>
                   </div>
-                  :
-                  <Link href="/">
-                    <a>
-                      <div title='Home' className='flex items-center justify-center rounded-full p-2 lg:p-3 bg-[#181A18] w-12 h-12 md:w-16 md:h-16 mr-2 drop-shadow-2xl border-white border-2 hover:border-green-500 transition-all duration-200 cursor-pointer' onClick={scrollToGallery}>
-                        <i className={`"uil uil-home text-3xl md:text-4xl text-white`}></i>
-                      </div>
-                    </a>
-                  </Link>
-              }
-              <div className='flex items-center justify-center rounded-full p-2 lg:p-3 bg-[#181A18] w-12 h-12 md:w-16 md:h-16 mr-2 drop-shadow-2xl border-white border-2 hover:border-blue-500 transition-all duration-200 cursor-pointer' onClick={() => setShowPictureofDay(!showPictureofDay)}>
-                <i className={`${showPictureofDay ? 'uil uil-eye-slash' : 'uil uil-eye'} text-3xl md:text-4xl text-white`}></i>
+                </Link>
+              )}
+              <div
+                className="flex items-center justify-center rounded-full p-2 lg:p-3 bg-[#181A18] w-12 h-12 md:w-16 md:h-16 mr-2 drop-shadow-2xl border-white border-2 hover:border-blue-500 transition-all duration-200 cursor-pointer"
+                onClick={() => setShowPictureofDay(!showPictureofDay)}
+              >
+                <i
+                  className={`${
+                    showPictureofDay ? "uil uil-eye-slash" : "uil uil-eye"
+                  } text-3xl md:text-4xl text-white`}
+                ></i>
               </div>
-              <div className='flex items-center justify-center rounded-full p-2 lg:p-3 bg-[#181A18] w-12 h-12 md:w-16 md:h-16 drop-shadow-2xl border-white border-2 hover:border-red-500 transition-all duration-200 cursor-pointer' onClick={(e) => onLikeClick(imageResponse.hdurl, e)}>
-                <i className={`${isLiked ? 'uis uis-heart-alt text-red-900' : 'uil uil-heart text-white'} text-3xl md:text-4xl transition-colors`}></i>
+              <div
+                className="flex items-center justify-center rounded-full p-2 lg:p-3 bg-[#181A18] w-12 h-12 md:w-16 md:h-16 drop-shadow-2xl border-white border-2 hover:border-red-500 transition-all duration-200 cursor-pointer"
+                onClick={(e) => onLikeClick(imageResponse.hdurl, e)}
+              >
+                <i
+                  className={`${
+                    isLiked
+                      ? "uis uis-heart-alt text-red-900"
+                      : "uil uil-heart text-white"
+                  } text-3xl md:text-4xl transition-colors`}
+                ></i>
               </div>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
 export default PictureOfDay;
--- pages/error/index.tsx
+++ pages/error/index.tsx
@@ -1,42 +1,40 @@
 import React from 'react';
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import Head from 'next/head';
 import Link from 'next/link';
 
 // sometimes api takes a while that the call gets timed out. 
 // since the index page relies on an image, this needs to be done so no error in production page 
 const Error = () => {
-  return (
-    <>
-      <Head>
-        <title>Spacetrest: API Timeout Error</title>
-        <meta name="description" content="Images about the universe" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
+  return <>
+    <Head>
+      <title>Spacetrest: API Timeout Error</title>
+      <meta name="description" content="Images about the universe" />
+      <link rel="icon" href="/favicon.png" />
+    </Head>
 
-      <div className='h-screen relative'>
+    <div className='h-screen relative'>
 
-        <Image src="/error.gif" alt='rotating moon' layout='fill' objectFit='cover' priority={true} quality={100} />
+      <Image src="/error.gif" alt='rotating moon' layout='fill' objectFit='cover' priority={true} quality={100} />
 
-        <div className="container mx-auto h-screen">
-          <div className='absolute top-1/2 transform -translate-y-1/2'>
-            <h1 className='text-4xl md:text-8xl text-center text-white font-bold'>API call timed out.</h1>
-            <p className='text-white text-center md:text-left text-lg mb-3'>Seems that the NASA APOD API is slow right now. Please try again later!</p>
-            <Link href="/">
-              <a>
-                <p className="uil uil-home text-center md:text-left text-4xl text-white cursor-pointer"></p>
-              </a>
-            </Link>
-          </div>
-          <div className="absolute bottom-[5%] left-1/2 transform -translate-x-1/2 cursor-pointer">
-            <a href='https://github.com/shanerrr/Spacetrest'>
-              <Image src="/github.png" alt='github logo' height={25} width={25} objectFit='cover' priority={true} quality={100} />
-            </a>
-          </div>
+      <div className="container mx-auto h-screen">
+        <div className='absolute top-1/2 transform -translate-y-1/2'>
+          <h1 className='text-4xl md:text-8xl text-center text-white font-bold'>API call timed out.</h1>
+          <p className='text-white text-center md:text-left text-lg mb-3'>Seems that the NASA APOD API is slow right now. Please try again later!</p>
+          <Link href="/">
+
+            <p className="uil uil-home text-center md:text-left text-4xl text-white cursor-pointer"></p>
+
+          </Link>
+        </div>
+        <div className="absolute bottom-[5%] left-1/2 transform -translate-x-1/2 cursor-pointer">
+          <a href='https://github.com/shanerrr/Spacetrest'>
+            <Image src="/github.png" alt='github logo' height={25} width={25} objectFit='cover' priority={true} quality={100} />
+          </a>
         </div>
       </div>
-    </>
-  )
+    </div>
+  </>;
 }
 
 export default Error;
