bd8ff1cac04c149acc5318b50f26b0b2f14715f6
Shane Goonasekera <theshanukg@gmail.com>
2023-06-10 22:57:15 -0600

    upgraded pacvkages

--- services/image.ts
+++ services/image.ts
@@ -1,28 +1,34 @@
-import axios from 'axios';
+import axios from "axios";
 
 export class ImageService {
-
   uri: string;
   config: any;
 
   constructor() {
-    this.uri = 'https://api.nasa.gov';
+    this.uri = "https://api.nasa.gov";
     this.config = {
-      pictureOfDay: '/planetary/apod' + `?api_key=${process.env.NEXT_PUBLIC_NASA_API_KEY}`,
-    }
+      pictureOfDay:
+        "/planetary/apod" + `?api_key=${process.env.NEXT_PUBLIC_NASA_API_KEY}`,
+    };
   }
 
   /**
-   * @param startDate optional param 
-   * @param endDate optional param 
+   * @param startDate optional param
+   * @param endDate optional param
    * @returns object of image
    */
-  getPictureOfDay(startDate: string | null = null, endDate: string | null = null) {
+  getPictureOfDay(
+    startDate: string | null = null,
+    endDate: string | null = null
+  ) {
     return axios({
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-      timeout: startDate ? 60000 : 15500,
-      url: this.uri + this.config.pictureOfDay + `${startDate ? `&start_date=${startDate}&end_date=${endDate}` : ''}`
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      timeout: 4000,
+      url:
+        this.uri +
+        this.config.pictureOfDay +
+        `${startDate ? `&start_date=${startDate}&end_date=${endDate}` : ""}`,
     });
   }
 
@@ -32,10 +38,10 @@ export class ImageService {
    */
   getSpecificImageOfDay(date: string | string[]) {
     return axios({
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
       timeout: 10000,
-      url: this.uri + this.config.pictureOfDay + `&date=${date}`
+      url: this.uri + this.config.pictureOfDay + `&date=${date}`,
     });
   }
 }
